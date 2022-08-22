1d1a42774e2e78f469d92f5f150ee83e2633f7f8
dgoldsb <dgoldsb@live.nl>
2022-08-22 22:11:05 +0200

    Day five with regex

---
+++ aoc/2015/day_5.py
@@ -0,0 +1,37 @@
+"""Solve day 5."""
+import re
+
+from aoc.helpers import time_it
+
+
+DUPLICATE = re.compile(r"(.)\1")
+AEIOU = re.compile(r"[aieou].*[aieou].*[aieou]")
+AB_CD_PQ_XY = re.compile(r"ab|cd|pq|xy")
+
+
+def _is_nice(string) -> bool:
+    return (
+        re.search(DUPLICATE, string) is not None
+        and re.search(AEIOU, string) is not None
+        and re.search(AB_CD_PQ_XY, string) is None
+    )
+
+
+XY_XY = re.compile(r"([a-z][a-z]).*\1")
+EFE = re.compile(r"([a-z]).\1")
+
+
+def _is_new_nice(string) -> bool:
+    return re.search(XY_XY, string) is not None and re.search(EFE, string) is not None
+
+
+@time_it
+def part_a(input_: str):
+    """How many strings are nice?"""
+    return len(list(filter(lambda s: _is_nice(s), iter(input_.split("\n")))))
+
+
+@time_it
+def part_b(input_: str):
+    """How many strings are nice under these new rules?"""
+    return len(list(filter(lambda s: _is_new_nice(s), iter(input_.split("\n")))))
