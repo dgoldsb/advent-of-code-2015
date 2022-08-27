9622b1e3109503c7efbcb0a2436784baa8d91b23
dgoldsb <dgoldsb@live.nl>
2022-08-27 17:14:30 +0200

    Add slow 6b

--- aoc/2015/day_6.py
+++ aoc/2015/day_6.py
@@ -1,4 +1,6 @@
 """Solve day 6."""
+from collections import defaultdict
+
 from aoc.helpers import ints, time_it
 
 
@@ -32,3 +34,32 @@ def part_a(input_: str):
             case _:
                 pass
     return len(on_lights)
+
+
+@time_it
+def part_b(input_: str):
+    """What is the total brightness?"""
+    # TODO: To Rust, was 3-4 seconds.
+    on_lights = defaultdict(int)
+    for line in input_.split("\n"):
+        edges = tuple(ints(line))
+        match line:
+            case str() as string if "toggle" in string:
+                for i in range(edges[0], edges[2] + 1):
+                    for j in range(edges[1], edges[3] + 1):
+                        tuple_ = (i, j)
+                        on_lights[tuple_] += 2
+            case str() as string if "turn on" in string:
+                for i in range(edges[0], edges[2] + 1):
+                    for j in range(edges[1], edges[3] + 1):
+                        tuple_ = (i, j)
+                        on_lights[tuple_] += 1
+            case str() as string if "turn off" in string:
+                for i in range(edges[0], edges[2] + 1):
+                    for j in range(edges[1], edges[3] + 1):
+                        tuple_ = (i, j)
+                        on_lights[tuple_] = max(0, on_lights[tuple_] - 1)
+            case _:
+                pass
+    return sum(on_lights.values())
+
